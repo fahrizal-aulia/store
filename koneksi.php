5cb3dbd8e02346a2d05456023665924875b3e5d2
fahrizal-aulia <fahrizalaulia@gmail.com>
2024-01-10 20:46:11 +0700

    delete fix

--- koneksi.php
+++ koneksi.php
@@ -9,7 +9,7 @@ if ($koneksi->connect_error) {
     // echo "Koneksi database berhasil! <br><br>";
     // $query = "SELECT * FROM tbl_products";
     // $query = "SELECT product, status, nama_category FROM tbl_products INNER JOIN tbl_category_product ON tbl_products.id_category = tbl_category_product.id;";
-    $query = "SELECT tbl_products.product, tbl_products.status, tbl_category_product.nama_category FROM tbl_products INNER JOIN tbl_category_product ON tbl_products.id_category = tbl_category_product.id;";
+    $query = "SELECT tbl_products.id,tbl_products.product, tbl_products.status, tbl_category_product.nama_category FROM tbl_products INNER JOIN tbl_category_product ON tbl_products.id_category = tbl_category_product.id;";
 
     $query1 = "SELECT * FROM tbl_category_product";
     $result = $koneksi->query($query);
